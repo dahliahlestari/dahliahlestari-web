271767d355fe052d04c8d071c9cee76f972484c7
Dahliah Lestari <dahliahlestari@Dahliahs-MacBook-Air.local>
2026-05-20 00:56:27 +0700

    udpate4

--- src/components/Software.jsx
+++ src/components/Software.jsx
@@ -7,11 +7,11 @@ const Software = () => {
   return (
     <section className="software fade-in">
       <div className="software-skill-label">
-        <span className="bullet-diamond">♦</span>
+        <span className="bullet-diamond">・</span>
         <span className="skill-text">Skill</span>
       </div>
       <div className="container software-container">
-        
+
         <div className="software-text-bg">
           <div className="text-row">PHOTOSHOP</div>
           <div className="text-row outline">ILLUSTRATOR</div>
@@ -21,22 +21,22 @@ const Software = () => {
         </div>
 
         <div className="software-images">
-             <div className="sw-img-card card-1">
-                <div 
-                  className="placeholder-merch"
-                  style={{ backgroundImage: `url(${weImage})` }}
-                ></div>
-             </div>
-             <div className="sw-img-card card-2">
-                <div 
-                  className="placeholder-merch abstract"
-                  style={{ backgroundImage: `url(${artboard2})` }}
-                ></div>
-             </div>
+          <div className="sw-img-card card-1">
+            <div
+              className="placeholder-merch"
+              style={{ backgroundImage: `url(${weImage})` }}
+            ></div>
+          </div>
+          <div className="sw-img-card card-2">
+            <div
+              className="placeholder-merch abstract"
+              style={{ backgroundImage: `url(${artboard2})` }}
+            ></div>
+          </div>
         </div>
       </div>
       <div className="software-bottom-btn">
-         <div className="show-more-btn">Show More Work</div>
+        <div className="show-more-btn">Show More Work</div>
       </div>
     </section>
   );
